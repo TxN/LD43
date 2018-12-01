b4d5dc42fb5a199f8d8c27536d250178a42db882
Anton <AimII@yandex.ru>
2018-12-01 16:49:05 +0600

    scriptale object asset create script

---
+++ Assets/Scripts/Editor/CreateEventAssetAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateLibratyAssetAction {
+	[MenuItem("Assets/Create/Tileset Resource Asset")]
+	public static void CreateMyAsset() {
+        ScriptableObject asset = null;// ScriptableObject.CreateInstance<ResourceLibrary>();
+
+		AssetDatabase.CreateAsset(asset, "Assets/Resources/ResourceLibrary_new.asset");
+		AssetDatabase.SaveAssets();
+
+		EditorUtility.FocusProjectWindow();
+		Selection.activeObject = asset;
+	}
+}
