773d335a4c6d97200d23fb991d570f60ad08baf2
rklipach <klipach.roman@gmail.com>
2018-12-01 17:59:53 +0700

    Update Plane state

--- Assets/Scripts/Events/Events.cs
+++ Assets/Scripts/Events/Events.cs
@@ -1,4 +1,14 @@
 ﻿
 namespace EventSys {
+    public struct Event_Plane_FlyIn
+    {
+    }
 
+    public struct Event_Plane_OnTarget
+    {
+    }
+
+    public struct Event_Plane_FlyOut
+    {
+    }
 }
--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EventSys;
 
 public class PlayerLogic : MonoBehaviour {
     public GameObject Plane = null;
+    public float PlaneOnTargetDelta = 5f;
     public float MaxSpawnRadius = 1500;
     public float MinSpawnRadius = 1000;
 
@@ -15,4 +17,16 @@ public class PlayerLogic : MonoBehaviour {
 
     StoryMapPoint _currentPoint = null;
 
+    PlaneState _state = PlaneState.Ready;
+
+	private void Update()
+	{
+        float distance = Vector3.Distance(transform.position, _currentPoint.transform.position);
+        if (_state == PlaneState.FlyIn && distance <= PlaneOnTargetDelta) {
+            _state = PlaneState.FlyOut;
+            EventManager.Fire<Event_Plane_OnTarget>(new Event_Plane_OnTarget());
+        } else if (_state == PlaneState.FlyOut && distance > PlaneOnTargetDelta) {
+            EventManager.Fire<Event_Plane_FlyOut>(new Event_Plane_FlyOut());
+        }
+	}
 }
