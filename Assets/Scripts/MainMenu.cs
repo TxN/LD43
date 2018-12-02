7790066c412f2c978aaba5dab7e9098563c86a58
Anton <AimII@yandex.ru>
2018-12-02 13:37:23 +0600

    Ready to png insertion

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public Button PlayButton = null;
+    public Button QuitButton = null;
+    public FadeScreen Fader = null;
+
+    void Start() {
+        PlayButton.onClick.AddListener(Play);
+        QuitButton.onClick.AddListener(Quit);
+    }
+
+    void Play() {
+        Fader.gameObject.SetActive(true);
+        Fader.FadeBlack(0.5f);
+        Invoke("Load", 0.6f);
+    }
+
+    void Load() {
+        SceneManager.LoadScene("PreGame");
+    }
+
+    void Quit() {
+        Application.Quit();
+    }
+}
